6d6f575d8296e9963465bf0e26b2d423257edae8
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-07-01 18:54:06 +0700

    hero
    and fix Call to action button

--- src/layout/pages/HomePage/Hero/Hero.js
+++ src/layout/pages/HomePage/Hero/Hero.js
@@ -1,22 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
 import clsx from "clsx";
 
-import styles from "./Hero.module.scss";
 import images from "~/assets/images";
+import CallToActionButton from "~/layout/components/CallToActionButton/CallToActionButton";
+import styles from "./Hero.module.scss";
 
 Hero.propTypes = {};
 
 function Hero(props) {
   return (
     <div className={clsx(styles.wrapper)}>
-      <div className={clsx(styles.title)}>Little Lemon</div>
-      <div className={clsx(styles.subTitle)}>Chicago</div>
-      <button>Reserve a Table</button>
-      <div className={clsx(styles.slogan)}>
-        A Taste of Greece in Every Bite!
+      <div className={clsx(styles.textContainer)}>
+        <div className={clsx(styles.title)}>Little Lemon</div>
+        <div className={clsx(styles.subTitle)}>Chicago</div>
+        <CallToActionButton>Reserve a Table</CallToActionButton>
+        <div className={clsx(styles.slogan)}>
+          A Taste of Greece in Every Bite!
+        </div>
       </div>
-      <img className={clsx(styles.heroPicture)} src={images.hero} alt="" />
+      <div className={clsx(styles.imgCrop)}>
+        <img className={clsx(styles.heroImg)} src={images.hero} alt="" />
+        <div className={clsx(styles.overlay)}></div>
+      </div>
     </div>
   );
 }
