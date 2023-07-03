373349283b7b2641910f42f8862eff3772a1d26c
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-07-03 17:46:43 +0700

    Highlight component

---
+++ src/layout/pages/HomePage/Highlight/Highlight.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+
+import styles from "./Highlight.module.scss";
+import CallToActionButton from "~/layout/components/CallToActionButton/CallToActionButton";
+import SpecialOrder from "~/layout/components/SpecialOrder/SpecialOrder";
+import Text from "~/layout/components/Text/Text";
+import dishes from "~/data/dishes";
+
+Highlight.propTypes = {};
+
+function Highlight(props) {
+  const specialDish = dishes.filter((dish) => dish.special);
+  const topDish = specialDish.splice(0, 3);
+  console.log(topDish);
+  return (
+    <div className={clsx(styles.wrapper)}>
+      <div className={clsx(styles.header)}>
+        <Text title>Special</Text>
+        <CallToActionButton>Online Menu</CallToActionButton>
+      </div>
+      {topDish.map((dish, idx) => (
+        <div key={idx} className={clsx(styles.content)}>
+          <SpecialOrder dish={dish} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Highlight;
