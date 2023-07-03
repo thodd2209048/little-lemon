00fe180816d34ba7e56e53aaea36a8cd916c621d
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-07-03 15:25:04 +0700

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,9 @@ const images = {
   yellowFullLogo: require("./Asset-16@4x.png"),
   shoppingBasket: require("./shopping-basket.svg").default,
   hero: require("./hero.jpg"),
+  greekSalad: require("./dish001.jpg"),
+  moussaka: require("./dish002.jpg"),
+  spanakopita: require("./dish003.jpg"),
 };
 
 export default images;
