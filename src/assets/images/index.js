a8ada03650d6c20af1ae3c283fb8d43d923bc598
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-07-04 08:25:39 +0700

    update assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,6 +5,10 @@ const images = {
   dish001: require("./dish001.jpg"),
   dish002: require("./dish002.jpg"),
   dish003: require("./dish003.jpg"),
+  cus001: require("./cus001.jpg"),
+  cus002: require("./cus002.jpg"),
+  cus003: require("./cus003.jpg"),
+  star: require("./Star.svg").default,
 };
 
 export default images;
