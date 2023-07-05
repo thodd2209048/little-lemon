f2cb5449fce1e3da0f197bc2f3fce3f847eafad8
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-07-05 11:53:21 +0700

    footer component

--- src/layout/components/Footer/Footer.js
+++ src/layout/components/Footer/Footer.js
@@ -1,9 +1,52 @@
-import React from "react";
+import clsx from "clsx";
+import { Col, Container, NavLink, Row } from "react-bootstrap";
+
+import styles from "./Footer.module.scss";
+import images from "~/assets/images";
+import Text from "../Text/Text";
+import { Link } from "react-router-dom";
 
 Footer.propTypes = {};
 
 function Footer(props) {
-  return <>Footer</>;
+  return (
+    <Container fluid className={clsx(styles.wrapper)}>
+      <Container>
+        <footer className={clsx(styles.container)}>
+          <div className={clsx(styles.imgArea)}>
+            <img src={images.logoVerticalYellow} alt="" />
+          </div>
+          <div className={clsx(styles.linkArea)}>
+            <div className={clsx(styles.linkCol)}>
+              <span cardTitle className={clsx(styles.linkColTitle)}>
+                Doormat Navigation
+              </span>
+              <NavLink to={"/"}>Home</NavLink>
+              <NavLink to={"/about"}>About</NavLink>
+              <NavLink to={"/menu"}>Menu</NavLink>
+              <NavLink to={"/reservations"}>Reservations</NavLink>
+              <NavLink to={"/order-online"}>Order Online</NavLink>
+              <NavLink to={"/login"}>Login</NavLink>
+            </div>
+            <div className={clsx(styles.linkCol)}>
+              <span className={clsx(styles.linkColTitle)}>Contact</span>
+              <NavLink to={"/contact-address"}>Address</NavLink>
+              <NavLink to={"/contact-phone"}>Phone number</NavLink>
+              <NavLink to={"/contact-email"}>Email</NavLink>
+            </div>
+            <div className={clsx(styles.linkCol)}>
+              <span className={clsx(styles.linkColTitle)}>
+                Social Media Links
+              </span>
+              <NavLink to={"/social-address"}>Address</NavLink>
+              <NavLink to={"/social-phone"}>Phone number</NavLink>
+              <NavLink to={"/social-email"}>Email</NavLink>
+            </div>
+          </div>
+        </footer>
+      </Container>
+    </Container>
+  );
 }
 
 export default Footer;
