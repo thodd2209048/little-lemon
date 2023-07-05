4200cec542ce7f48baeb56aecbb003c450d20789
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-07-05 19:57:57 +0700

    footer component

--- src/layout/components/Footer/Footer.js
+++ src/layout/components/Footer/Footer.js
@@ -12,38 +12,51 @@ function Footer(props) {
   return (
     <Container fluid className={clsx(styles.wrapper)}>
       <Container>
-        <footer className={clsx(styles.container)}>
-          <div className={clsx(styles.imgArea)}>
+        <Row>
+          {/* <footer className={clsx(styles.container)}> */}
+          <div
+            className={clsx(styles.imgArea, "d-none", "d-md-block", "col-md-2")}
+          >
             <img src={images.logoVerticalYellow} alt="" />
           </div>
-          <div className={clsx(styles.linkArea)}>
-            <div className={clsx(styles.linkCol)}>
-              <span cardTitle className={clsx(styles.linkColTitle)}>
-                Doormat Navigation
+          <div className={clsx("d-none", "d-md-block", "col-md-1")}></div>
+          {/* <div className={clsx(styles.linkArea)}> */}
+          <div className={clsx(styles.linkCol, "col-md-3")}>
+            <span cardTitle className={clsx(styles.linkColTitle)}>
+              Doormat Navigation
+            </span>
+            <ul className={clsx(styles.linkList)}>
+              <span>
+                <Link to={"/"}>Home</Link>
               </span>
-              <NavLink to={"/"}>Home</NavLink>
-              <NavLink to={"/about"}>About</NavLink>
-              <NavLink to={"/menu"}>Menu</NavLink>
-              <NavLink to={"/reservations"}>Reservations</NavLink>
-              <NavLink to={"/order-online"}>Order Online</NavLink>
-              <NavLink to={"/login"}>Login</NavLink>
-            </div>
-            <div className={clsx(styles.linkCol)}>
-              <span className={clsx(styles.linkColTitle)}>Contact</span>
-              <NavLink to={"/contact-address"}>Address</NavLink>
-              <NavLink to={"/contact-phone"}>Phone number</NavLink>
-              <NavLink to={"/contact-email"}>Email</NavLink>
-            </div>
-            <div className={clsx(styles.linkCol)}>
-              <span className={clsx(styles.linkColTitle)}>
-                Social Media Links
-              </span>
-              <NavLink to={"/social-address"}>Address</NavLink>
-              <NavLink to={"/social-phone"}>Phone number</NavLink>
-              <NavLink to={"/social-email"}>Email</NavLink>
-            </div>
+              <Link to={"/about"}>About</Link>
+              <Link to={"/menu"}>Menu</Link>
+              <Link to={"/reservations"}>Reservations</Link>
+              <Link to={"/order-online"}>Order Online</Link>
+              <Link to={"/login"}>Login</Link>
+            </ul>
           </div>
-        </footer>
+          <div className={clsx(styles.linkCol, "col-md-3")}>
+            <span className={clsx(styles.linkColTitle)}>Contact</span>
+            <ul className={clsx(styles.linkList)}>
+              <Link to={"/contact-address"}>Address</Link>
+              <Link to={"/contact-phone"}>Phone number</Link>
+              <Link to={"/contact-email"}>Email</Link>
+            </ul>
+          </div>
+          <div className={clsx(styles.linkCol, "col-md-3")}>
+            <span className={clsx(styles.linkColTitle)}>
+              Social Media Links
+            </span>
+            <ul className={clsx(styles.linkList)}>
+              <Link to={"/social-address"}>Address</Link>
+              <Link to={"/social-phone"}>Phone number</Link>
+              <Link to={"/social-email"}>Email</Link>
+            </ul>
+          </div>
+          {/* </div> */}
+          {/* </footer> */}
+        </Row>
       </Container>
     </Container>
   );
