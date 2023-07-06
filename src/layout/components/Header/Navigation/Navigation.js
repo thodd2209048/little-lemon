5f807950dab3f01daffb3ade7fd67343e906805a
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-07-06 19:44:38 +0700

    fix header
    add some margin
    fix brand image

--- src/layout/components/Header/Navigation/Navigation.js
+++ src/layout/components/Header/Navigation/Navigation.js
@@ -18,7 +18,7 @@ function Navigation(props) {
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="#home" className={clsx(styles.logo)}>
             <img
               className={clsx(styles.logo)}
               src={images.yellowFullLogo}
@@ -27,7 +27,7 @@ function Navigation(props) {
           </Navbar.Brand>
           {size.width < 992 && <Cart />}
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className={clsx(styles.linkList, "me-auto")}>
               <Nav.Link to="/menu">Menu</Nav.Link>
               <Nav.Link to="/reservation">Reservation</Nav.Link>
               <Nav.Link to="/about">About</Nav.Link>
